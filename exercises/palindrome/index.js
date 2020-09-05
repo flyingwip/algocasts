83b877370463f648b4de08b14b404960c11fad8a
Martijn Wip <flyingwip@gmail.com>
2020-09-05 10:20:08 +0200

    the palindrome and reversestring algo's

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,39 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {}
 
-module.exports = palindrome;
+// by compare string reverse
+// palindrome = (str) => {
+//   return str === str.split('').reverse().join('')
+// }
+
+left_right = (str, splitter) => {
+  let left = str.slice(0, splitter / 2)
+  let right = str.slice((splitter / 2) * -1)
+  return [left, right]
+}
+
+// by splitting and compare splits
+// palindrome = (str) => {
+//   let splitter = str.length
+//   if (str.length % 2) {
+//     lgth = str.length - 1
+//   }
+//   const [left, right] = left_right(str, splitter)
+//   return left === right.split('').reverse().join('')
+// }
+
+// by using the every function
+palindrome = (str) => {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  })
+}
+
+// palindrome('tenet')
+// palindrome('abba')
+console.log(palindrome('tenet'))
+// console.log(palindrome('abba'))
+
+module.exports = palindrome
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,44 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// as a classic function
+// function reverse(str) {
+//   return str.split('').reverse().join().replace(/,/g, '')
+// }
 
-module.exports = reverse;
+//as an arrow function
+// reverse = (str) => str.split('').reverse().join('')
+
+// function reverse(str) {
+//   console.log(str)
+// }
+
+// option 2
+reverse = (str) => {
+  debugger
+  let reversed = ''
+  for (character of str) {
+    reversed = character + reversed
+  }
+  return reversed
+}
+
+reverse('hallo')
+
+// option 3 with the reduce function!
+// reverse = (str) => {
+//   return str.split('').reduce((rev, char) => char + rev, '')
+// }
+
+module.exports = reverse
+
+// var i = 0
+
+// setInterval(() => {
+//   console.log('iteration: ' + i)
+//   i++
+// }, 1000)
